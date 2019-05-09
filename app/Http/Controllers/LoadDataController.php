481e933cb6be742f5e408e7343304565b795a872
IrisChu <contact.iris.chu@gmail.com>
2019-05-09 00:54:42 -0700

    Finish csv data import

--- app/Http/Controllers/LoadDataController.php
+++ app/Http/Controllers/LoadDataController.php
@@ -9,8 +9,11 @@ class LoadDataController extends BaseController
   {
     $fileName = "/name_data.csv";
     $row = 0;
-    if (($handle = fopen ( public_path () . $fileName, 'r' )) !== FALSE) {
-  		while ( ($line = fgetcsv ( $handle, 1000, ',' )) !== FALSE ) {
+    if (($handle = fopen(public_path () . $fileName, 'r')) !== FALSE) {
+      DB::table('names')->delete();
+      DB::table('characteristic_name')->delete();
+
+  		while (($line = fgetcsv($handle, 1000, ',' )) !== FALSE) {
           $row++;
           if($row == 1) {
             continue;
@@ -18,30 +21,45 @@ class LoadDataController extends BaseController
           $gender = $line[0];
           $pronunciation = $line[1];
           $name = $line[2];
-          $characteristic = $line[3];
+          $characteristics = $line[4];
         // skip for now
-        //  $meaning = $line[4];
-          $famousPerson = $line[5];
+        //  $meaning = $line[5];
+          $famousPerson = $line[6];
 
-          $SQL = "SELECT * FROM names where name = '$name' limit 1";
-          $existingName = DB::select($SQL);
-
-           if($existingName) {
-            // $SQL = "UPDATE names SET pronunciation = $pronunciation, gender = $gender, updated_at = NOW()) WHERE id = $nameID";
-            // mysqli_query($con, $SQL);
-            //
-            // $SQL = "UPDATE characteristic_name SET characteristic_id = (SELECT id FROM characteristics WHERE characteristic = $characteristic) WHERE name_id = $nameID";
-            // mysqli_query($con, $SQL);
-          } else {
-            $SQL = "INSERT INTO names (name, pronunciation, gender, created_at, updated_at) VALUES('$name', '$pronunciation', '$gender', NOW(), NOW())";
-            $result = DB::insert($SQL);
-            // DB::insert($SQL);\
-
-            // $SQL = "INSERT INTO characteristic_name (characteristic_id, name_id, created_at, updated_at) VALUES( (SELECT id FROM characteristics WHERE characteristic = $characteristic), $nameID, NOW(), NOW())"
-            // DB::insert($SQL);
+          if(!isset($name) || empty($name)) {
+            continue;
           }
+
+          $nameID = DB::table('names')->insertGetId(
+              ['name' => $name,
+              'pronunciation' => $pronunciation,
+              'gender' => $gender,
+              'created_at' => NOW(),
+              'updated_at' => NOW()
+            ]
+          );
+
+          $characteristics = explode(",", str_replace(' ', '', $characteristics));
+
+          foreach ($characteristics as $characteristic) {
+            $characteristicID = DB::table('characteristics')->where('characteristic', $characteristic)->value('id');
+
+            if(!isset($characteristicID) || empty($characteristicID)) {
+              continue;
+            }
+
+            $characteristicNameID = DB::table('characteristic_name')->insertGetId(
+                ['characteristic_id' => $characteristicID,
+                'name_id' => $nameID,
+                'created_at' => NOW(),
+                'updated_at' => NOW()
+              ]
+            );
+          }
+
   		}
   		fclose ( $handle );
+      echo 'done';
   	}
   }
 }
