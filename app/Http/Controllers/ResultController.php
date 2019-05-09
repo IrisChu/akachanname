2fadebf3d2aee8e3a731a723801a0757e255162c
IrisChu <contact.iris.chu@gmail.com>
2019-05-09 01:14:19 -0700

    Update results so it grabs one random name of the selected characteristic and gender

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -1,21 +1,21 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class ResultController extends BaseController
 {
   public function show($gender, $property)
   {
-    // todo create database
-    // get actual values depending on values
-    if($gender == 'girl') {
-      $names = array("Value" => "Kylie", "Amanda", "Katherine");
+    $name = DB::table('names')
+                ->join('characteristic_name', 'names.id', '=', 'characteristic_name.name_id')
+                ->join('characteristics', 'characteristics.id', '=', 'characteristic_name.characteristic_id')
+                ->select('names.name', 'names.pronunciation')
+                ->where('names.gender', $gender)
+                ->where('characteristics.characteristic', $property)
+                ->inRandomOrder()
+                ->first();
 
-    } else {
-      $names = array("James", "Mark", "Matthew");
-    }
-
-    return view('result')->with('type', $gender)->with('names', $names);
-
+    return view('result')->with('type', $gender)->with('name', $name);
   }
 }
